3b933c525602adfa6fb492bfc3a34ce40a70e988
Nolan Charpentier <nolan61250@gmail.com>
2024-11-07 17:00:11 +0100

    Update portfolio contact

--- portfolio.php
+++ portfolio.php
@@ -1,37 +1,39 @@
-<?php
-echo '<!DOCTYPE html>';
-echo '<html>';
-echo '<head>';
-echo '<title>Mon Portfolio</title>';
-echo '<link rel="stylesheet" type="text/css" href="assets/css/portfolio.css">';
-echo '</head>';
-echo '<body>';
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>portfolio</title>
+  <link rel="stylesheet" href="./assets/css/portfolio.css">
+ 
+</head>
+<body>
 
+<?php
 require_once("C:/xampp/htdocs/YAML/yaml/yaml.php");
 $data = yaml_parse_file('portfolio.yaml');
 ?>
 <header>
-<nav class="navbar">
-  <ol>
-    <li><a href="#accueil">ACCUEIL</a></li>
-    <li><a href="#competences">COMPETENCES</a></li>
-    <li><a href="#realisations">REALISATIONS</a></li>
-    <li><a href="#formations">FORMATIONS</a></li>
-    <li><a href="#contact">CONTACT</a></li>
-  </ol>
-</nav>
+    <nav class="navbar">
+    <ol>
+        <li><a href="#accueil">ACCUEIL</a></li>
+        <li><a href="#competences">COMPETENCES</a></li>
+        <li><a href="#realisations">REALISATIONS</a></li>
+        <li><a href="#formations">FORMATIONS</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+    </ol>
+    </nav>
 </header>
 
 <?php
-echo "<section>";
-echo "<h1 id='accueil'>Accueil</h1>\n";
+echo "<section id='accueil'>";
+echo "<h1>Accueil</h1>\n";
 echo "<h2>".$data['accueil']['nom']." ".$data['accueil']['prenom']."</h2>\n";
 echo "<p>".$data['accueil']['accroche']."</p>\n";
 echo "<p>".$data['accueil']['presentation']."</p>\n";
 echo "</section>";
 
-echo "<section>";
-echo "<h1 id='competences'>Compétences</h1>\n";
+echo "<section id='competences'>";
+echo "<h1>Compétences</h1>\n";
 
 foreach ($data['competences']['domaines'] as $domaine) {
     echo "<h3>".$domaine['nom']."</h3>\n";
@@ -91,29 +93,58 @@ foreach ($data['competences']['domaines'] as $domaine) {
 
 echo "</section>";
 
-echo "<section>";
-echo "<h1 id='realisations'>Réalisations</h1>\n";
+echo "<section id='realisations'>";
+echo "<h1>Réalisations</h1>\n";
 echo "<h2>".$data['realisations']['titre']."</h2>\n";
 echo "<p>".$data['realisations']['description']."</p>\n";
 // ensuite mettre *illustration et *documents qui ne sont pas du texte
 // si plusieurs réalisations, mettre une boucle for
 echo "</section>";
 
-echo "<section>";
-echo "<h1 id='formations'>Formations</h1>\n";
+echo "<section id='formations'>";
+echo "<h1>Formations</h1>\n";
 foreach ($data['formations'] as $formation) {
     echo "<h3>".$formation['nom']." - ".$formation['etablissement']."</h3>\n";
     echo "<p>".$formation['date_debut']."-" .$formation['date_fin']."</p>\n";
     echo "<p>".$formation['lieu']."</p>\n";
     echo "<p>".nl2br($formation['contenu'])."</p>\n";
 }
-echo "</section>";
-
-echo "<section>";
-echo "<h1 id='contact'>Contact</h1>\n";
-
-echo "</section>";
-
-echo '</body>';
-echo '</html>';
 ?>
+
+</section>
+
+<section id='contact'>
+    <div class="contactez-nous">
+        <h1>Contact</h1>
+        <p>Un problème, une question, envie de m'envoyer un message ? N’hésitez pas à utiliser ce formulaire pour prendre contact avec moi !</p>
+        <form action="" method="post">
+            <div>
+                <label for="nom">Votre nom</label>
+                <input type="text" id="nom" name="nom" placeholder="Martin" required>
+            </div>
+            <div>
+                <label for="email">Votre e-mail</label>
+                <input type="email" id="email" name="email" placeholder="monadresse@mail.com" required>
+            </div>
+            <div>
+                <label for="sujet">Quel est le sujet de votre message ?</label>
+                <select name="sujet" id="sujet" required>
+                    <option value="" disabled selected hidden>Choisissez le sujet de votre message</option>
+                    <option value="probleme-portfolio">Problème avec mon portfolio</option>
+                    <option value="question">Question à propos de moi</option>
+                    <option value="autre">Autre...</option>
+                </select>
+            </div>
+            <div>
+                <label for="message">Votre message</label>
+                <textarea id="message" name="message" placeholder="Bonjour, je vous contacte car...." required></textarea>
+            </div>
+            <div>
+                <button type="submit">Envoyer mon message</button>
+            </div>
+        </form>
+    </div>
+</section>
+
+</body>
+</html>
