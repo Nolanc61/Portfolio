6e736d7184897465fbbf5d601a06fb8c52e4631b
CharpentierNolan <nolan61250@gmail.com>
2024-11-27 15:00:04 +0100

    Update accueil.php

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,5 +1,8 @@
 <section id='accueil'>
-    <h2><?php echo $data['accueil']['nom'] . " " . $data['accueil']['prenom']; ?></h2>
-    <p><?php echo $data['accueil']['accroche']; ?></p>
-    <p><?php echo $data['accueil']['presentation']; ?></p>
+    <h2><?php echo $data['accueil']['nom'] . " " . $data['accueil']['prenom']; ?><br><br></h2>
+    <div id="gauche">
+        <p><?php echo $data['accueil']['accroche']; ?><br><br></p>
+        <p><?php echo $data['accueil']['presentation']; ?></p>
+    </div>
+    <img src="assets/images/mickey.jpg" alt="" id="droite">
 </section>
